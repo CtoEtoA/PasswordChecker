6300f5b344020dbb0daf9b1596abdb21dd7c4c18
Christian Anderson <64821990+CtoEtoA@users.noreply.github.com>
2026-02-17 21:19:45 +0000

    basic setup

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Django example",
+        "title": "Password Checker",
+        "subtitle": "Created for UofU CS3090",
+        "description": "This is a password checker that checks the strength of a password and provides feedback on how to improve it."
     }
     return render(request, "index.html", context)
