26cf1ecfaea406d7e886f0ac63ee38ffabfa5afb
Christian Anderson <64821990+CtoEtoA@users.noreply.github.com>
2026-02-18 22:05:52 +0000

    explicit request typing

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
-def index(request):
+def index(request: HttpRequest):
     context = {
         "title": "Password Checker",
         "subtitle": "Created for UofU CS3090",
